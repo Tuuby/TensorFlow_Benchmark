661a1a558eda30817d36979d11b6688355738937
Tuuby <tobiastrompell@aol.com>
2022-05-15 17:45:21 +0200

    Change benchmarking to multiple iterations and add code to save results in a file

--- main.py
+++ main.py
@@ -1,30 +1,52 @@
+import random
+import statistics
 import time
-import math
-
+import json
 import tensorflow as tf
+import csv
 
+# Define Benchmark variables
+operations = 100000
+iteration_count = 10
+
+# Start preparing benchmark data
 print("TensorFlow version: ", tf.__version__)
 print("Preparing benchmark data")
 
-length = 100000
+with open("matrices.json", "r") as openfile:
+    json_object = json.load(openfile)
 
+json_size = len(json_object)
 matrices = []
-for i in range(length):
-    values = []
-    for j in range(100):
-        values.append(math.sin(i + j))
-    matrix = tf.constant(values, shape=[10, 10])
+
+for i in range(json_size):
+    matrix = tf.constant(json_object[i], shape=[20, 20])
     matrices.append(matrix)
 
-halflength = len(matrices) // 2
-final = tf.constant([0] * 100, shape=[10, 10])
+matrix_count = len(matrices)
 
-print(f"Starting benchmark with {length} matrices")
-start = time.perf_counter()
+times = []
 
-for i in range(halflength):
-    result = tf.matmul(matrices[i], matrices[i + halflength])
+# Begin timer and actual benchmark
+for i in range(iteration_count):
+    print(f"[{i + 1}/{iteration_count}] Running benchmark iteration {i + 1} with {operations} operations...")
+    start = time.perf_counter()
 
-finish = time.perf_counter()
+    for x in range(operations):
+        result = tf.matmul(matrices[random.randrange(matrix_count)], matrices[random.randrange(matrix_count)])
 
-print(f"Benchmark finished in {finish - start:0.10f} seconds.")
+    finish = time.perf_counter()
+    time_taken = round(finish - start, 10)
+    times.append(time_taken)
+    print(f"...finished in {time_taken} seconds.\n--------------------------------------")
+
+# Process and present the results
+mean = statistics.mean(times)
+print(f"[Complete] {iteration_count} benchmarks finished.\nIdividual times in seconds: {times} | mean: {mean} seconds")
+
+with open("results.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["sep=,"])
+    for i in range(iteration_count):
+        writer.writerow([f"Iteration {i + 1}", f"{times[i]}s"])
+    writer.writerow(["Mean", f"{mean}s"])
